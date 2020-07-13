645f4a614894c3ff2d73341b937af53cb70b2df4
aguseche <48460721+aguseche@users.noreply.github.com>
2020-07-13 12:31:01 -0300

    Practica 7 Finalizada

---
+++ Practica7/ej5.php
@@ -0,0 +1,4 @@
+<!--Realizar una página donde carguemos en un formulario el nombre de usuario y clave de un cliente.
+Luego realizar una segunda página donde se creen dos variables de sesión. Y como última página
+crear una tercera en la cual se recuperen los valores almacenados en las variables de sesión
+anterior. -->
